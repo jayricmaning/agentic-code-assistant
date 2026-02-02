842b59ca3d982c42b8ddb84487c5ebf35a73af7d
Jayric <jricmaning@gmail.com>
2026-02-02 20:35:03 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -11,16 +12,17 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
+    
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
     
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents=args.user_prompt
-    )
+    model="gemini-2.5-flash", contents=messages
+)
 
-    print(f"User prompt: {args.user_prompt}")
+    print(f"User prompt: {messages}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
