098de618a82d9f3b829429fe2789c4d5d3a2f05e
Jayric <jricmaning@gmail.com>
2026-02-02 20:28:51 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+
 
 load_dotenv()
 
@@ -9,14 +11,16 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-    user_prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
     
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents=user_prompt
+        contents=args.user_prompt
     )
 
-    print(f"User prompt: {user_prompt}")
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
