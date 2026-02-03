dc005dd23f2db6f47b85c2dadd229a1e6e184225
Jayric <jricmaning@gmail.com>
2026-02-03 19:25:23 +0800

    Changes to be committed: 	new file:   calculator/main.py 	new file:   calculator/pkg/calculator.py 	new file:   calculator/pkg/render.py 	new file:   calculator/tests.py 	modified:   main.py

--- main.py
+++ main.py
@@ -15,17 +15,20 @@ def main():
     
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+    
     
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
     model="gemini-2.5-flash", contents=messages
 )
-
-    print(f"User prompt: {messages}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response:\n{response.text}")
+    if args.verbose:
+        print(f"User prompt: {messages}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Response:\n{response.text}")
+    print(print(f"Response:\n{response.text}"))
 
 
 if __name__ == "__main__":
